6c2cb18a16970766f04dce36cbc705f26cd912f9
Anish <anishujjodi@gmail.com>
2022-03-04 12:05:10 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', 
+  loadChildren:()=>import('./home/home.module').then(mod=>mod.HomeModule)},
+  {path:'favourite', 
+  loadChildren:()=>import('./favourite/favourite.module').then(mod=>mod.FavouriteModule)},
+  {path:'recentsearch',
+  loadChildren:()=>import('./recent/recent.module').then(mod=>mod.RecentModule)}
+  ,
+    { path: '',
+     pathMatch: 'full', 
+     loadChildren:()=>import('./home/home.module').then(mod=>mod.HomeModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
